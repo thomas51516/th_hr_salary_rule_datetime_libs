d1f3decc800c0f19d4c0d263b17106bed1de47bf
thomas51516 <thomas51516@gmail.com>
2020-03-30 18:58:54 +0000

    Add files via upload

---
+++ th_hr_salary_rule_datetime_libs/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'salary rule datetime libs',
+    'version' : '0.1.0',
+    'summary' : """
+        Ajouter le module dateteime à payroll
+    """,
+    'category' : 'Tools',
+    'author' : 'Thomas ATCHA',
+    'maintainer' : 'Zen Roots-Technologies',
+    'company': 'Zen Roots-Technologies',
+    'website' : 'https://www.roots-technologies.com',
+    'depends' : ['hr_payroll'],
+    'data' : [
+    ],
+
+    'images' : [],
+    'license' : 'AGPL-3',
+    'installable' : True,
+    'application' : True,
+    'auto_install' : False,
+}
